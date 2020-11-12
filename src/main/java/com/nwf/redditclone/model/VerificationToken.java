913e8dbcca066080852fa47a4fe6a894387b23fa
Nico <ncfortuin27@gmail.com>
2020-11-12 12:51:43 +0200

    implemented models

--- src/main/java/com/nwf/redditclone/model/VerificationToken.java
+++ src/main/java/com/nwf/redditclone/model/VerificationToken.java
@@ -1,4 +1,23 @@
 package com.nwf.redditclone.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class VerificationToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
+    private Instant expiryDate;
 }
--- src/main/java/com/nwf/redditclone/model/Vote.java
+++ src/main/java/com/nwf/redditclone/model/Vote.java
@@ -1,4 +1,27 @@
 package com.nwf.redditclone.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
 public class Vote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long voteId;
+    private VoteType voteType;
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "postId", referencedColumnName = "postId")
+    private Post post;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
 }
